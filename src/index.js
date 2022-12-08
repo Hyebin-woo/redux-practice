5b06d064cd51d255200e1fa493bcb8fa4ac85d71
Hyebin-Woo <dev.hyebin@gmail.com>
2022-12-08 21:41:39 +0900

    #1.2 액션

--- src/index.js
+++ src/index.js
@@ -5,17 +5,23 @@ const minus = document.getElementById("minus");
 const number = document.querySelector("span");
 
 //✅ Reducer는 data를 modify 해주는 함수로 reducer가 return하는 것은 application에 있는 data가 됨
-const countModifier = (count = 0) => {
-    count += 1;
-    count -= 1;
-
-    return count;
+const countModifier = (count = 0, action) => {
+    if (action.type === "ADD") {
+        return count + 1;
+    } else if (action.type === "MINUS") {
+        return count - 1;
+    } else {
+        return count;
+    }
 };
 
 // state application에서 바뀌는 data
 //✅ Store는 data를 저장하는 곳
 //✅ CreateStore는 reducer를 요구함.
 const countStore = createStore(countModifier);
+
+countStore.dispatch({ type: "ADD" });
+countStore.dispatch({ type: "MINUS" });
 
 // let count = 0;
 // number.innerText = count;
@@ -33,5 +39,5 @@ const countStore = createStore(countModifier);
 //     updateText();
 // };
 
-plus.addEventListener("click", handleAdd);
-minus.addEventListener("click", handleMinus);
+//plus.addEventListener("click", handleAdd);
+//minus.addEventListener("click", handleMinus);
